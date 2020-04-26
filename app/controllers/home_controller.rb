c053e2ae437f0893748e56dc2b21aca9c8d1b692
mcruz1@andrew.cmu.edu <mcruz1@andrew.cmu.edu>
2020-04-25 23:50:51 -0400

    basic admin dashboard; need to make generate payroll functionality work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @stores = Store.all.alphabetical
+    # @store_array = store.map{|store| [store.name, store.id] }
+  end
+
+  def generate_payroll
+    date_range = DateRange.new(date_range_params)
+    store = params[:store]
+    calc = PayRollCalculator.new(date_range)
+    report = PayRollCalculator.create_payrolls_for(store)
   end
 
   def about
@@ -9,6 +18,12 @@ class HomeController < ApplicationController
   end
 
   def privacy
+  end
+
+  private 
+
+  def date_range_params 
+    params.require(:date_range).permit(:start_date, :end_date)
   end
   
 end
