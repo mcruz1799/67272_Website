c4ac749726cfb9aa39253efad96522cc8d899aa2
mcruz1@andrew.cmu.edu <mcruz1@andrew.cmu.edu>
2020-05-04 12:33:23 -0400

    punch_clock controller method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,6 +11,20 @@ class HomeController < ApplicationController
     report = PayRollCalculator.create_payrolls_for(store)
   end
 
+  def punch_clock
+    if punch_in?
+      @shift = Shift.pending.for_employee(@employee).for_next_days(0).first
+      tc = TimeClock.new(@shift)
+      tc.start_shift_at()
+      redirect_to home_path, notice: "Successfully clocked in."
+    elsif punch_out?
+      @shift = Shift.started.for_employee(@employee).for_next_days(0).first
+      tc = TimeClock.new(@shift)
+      tc.end_shift_at()
+      redirect_to home_path, notice: "Successfully clocked out."
+    end
+  end
+
   def about
   end
 
@@ -26,4 +40,13 @@ class HomeController < ApplicationController
     params.require(:date_range).permit(:start_date, :end_date)
   end
   
+  def punch_in? 
+    employee = current_user
+    Shift.pending.for_employee(@employee).for_next_days(0).first.nil? ? false : true
+  end
+
+  def punch_out? 
+    employee = current_user
+    Shift.started.for_employee(@employee).for_next_days(0).first.nil? ? false : true
+  end
 end
