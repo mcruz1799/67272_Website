063015eecb0415ea48b3699607d2ab485c2722e8
mcruz1@andrew.cmu.edu <mcruz1@andrew.cmu.edu>
2020-05-04 19:08:08 -0400

    store report working

--- app/controllers/payroll_controller.rb
+++ app/controllers/payroll_controller.rb
@@ -4,10 +4,10 @@ class PayrollController < ApplicationController
     puts params[:date_range]
     start_date  = params[:date_range][:start_date].to_date
     end_date    = params[:date_range][:end_date].to_date
-    store       = Store.find(params[:date_range][:store_id])
+    @store       = Store.find(params[:date_range][:store_id])
     date_range = DateRange.new(start_date, end_date)
-    calc = PayrollCalculator.new(date_range)
-    @report = calc.create_payrolls_for(store)
+    @calc = PayrollCalculator.new(date_range)
+    @payrolls = @calc.create_payrolls_for(@store)
   end
 
   def employee_report
